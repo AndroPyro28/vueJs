c5f51812f3d98841f6f6f4ce18eb91804d0e3ca2
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-09 06:19:26 -0400

    conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,31 @@
 <template>
+  <!-- template is like react fragment  -->
 
-  <div>{{ `${greet} ${name}` }}</div>
+  <h2 v-if="number < 0">The Number is Negative</h2>
+  <h2 v-else-if="number === 0">The Number is Zero</h2>
+  <h2 v-else-if="number > 0">The Number is Positive</h2>
+  <h2 v-else>Not a number</h2>
 
-  <div v-html="channel"></div>
+  <template v-if="display">
+    <h2>Andro Eugenio VueJs</h2>
+  </template>
 
-  <div v-html="hack"></div> <!-- binding html attribute with text -->
+  <h2 v-show="showElement">Using V-show</h2>
+  <!-- v-show only set display:none to css properties if the current condition is not truthy  -->
 
-  <h2 :id="headingId">Heading</h2>  <!-- binding data into attributes-->
-  
-  <button :disabled="isDisabled" v-on:click="(e) => toggleDisabled(true, e)"> <!-- onClick function with methods-->
-    disabled button
-  </button>
-  <!-- short term @click="method"-->
-  <h3 v-bind:class="status">h3</h3>
-  <h2 v-bind:class='isPromoted && "promoted"'>Promoted Movie</h2>
-  <h2 v-bind:class="isSoldOut ? `sold-out` : `new` ">Movie</h2> <!-- conditional binding classes  -->
+  <h2 v-if="showElement">Using V-show</h2>
+  <!-- v-if  will completely inject to dom when condition is not truthy  -->
 
-  <h2 :class="['new', 'promoted']" > newly promoted movie </h2>
-  <h2 :class="[isPromoted && 'promoted', !isSoldOut ? `soldOut` : `new`]" > Array conditional movie </h2> <!-- array -->
-
-
-  <h2 :class="{
-    promoted: isPromoted,
-    new: !isSoldOut,
-    soldOut: !isSoldOut
-  }" >object conditional movie
-  </h2>
-
-
-  <h2 :style="{
-    color: highlightColor,
-    fontSize: `${headerSize}px`,
-  }" >Inline style</h2>
-
-  <h2 :style="headerStyleObject" >Inline style</h2>
-
-  <div :style="[baseStyleObject, successStyleObject]" > Success </div>
-  <div :style="[baseStyleObject, dangerStyleObject]" > Danger </div>
-
+  <!-- <h2> // long term
+    {{
+      number < 0 
+        ? `The number is negative`
+        : number === 0
+        ? `The number is zero`
+        : ` The number is positive` 
+    }}
+    {{ number }} 
+  </h2> -->
 </template>
 
 <script>
@@ -45,47 +33,10 @@ export default {
   name: "App",
   data() {
     return {
-      name: "andro",
-      greet: "hello",
-      channel: "<b>Codevolution</b>",
-      hack: `<button href='#' onclick="alert('you have been hacked')"> Win a prize </button>`,
-      headingId: "heading",
-      isDisabled: false,
-      status: "underline",
-      isPromoted: false,
-      isSoldOut: false,
-      highlightColor: 'orange',
-      headerSize: 50,
-      headerStyleObject: {
-        color: 'orange',
-        fontSize: `50px`,
-      },
-      baseStyleObject: {
-        fontSize: '50px',
-        padding: '10px',
-        textAlign:'start'
-      },
-      successStyleObject: {
-        color:'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid green',
-        padding:'50px',
-        textAlign:'center'
-      },
-      dangerStyleObject: {
-        color:'darkred',
-        backgroundColor:'red',
-        border:'1px solid darkred',
-        padding:'50px',
-        textAlign:'center'
-      }
+      number: 0,
+      display: true,
+      showElement: false,
     };
-  },
-  methods: {
-    toggleDisabled(bool, e) {
-      this.isDisabled = bool;
-      console.log(e.target.innerText);
-    },
   },
 };
 </script>
@@ -98,20 +49,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-
-.promoted {
-  font-style: italic;
-}
-.new {
-  color: green;
-}
-.soldOut {
-  color: red;
-  text-decoration: line-through ;
 }
 </style>
