25ab779a2b022a7bac9047d1389b919ec2e498b0
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-10 07:07:23 -0400

    list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
 <template>
-  <!-- template is like react fragment  -->
+  <!-- <h2 v-for="(name, index) in names" :key="index" >{{index}} {{name}}</h2> iterating in array of string -->
 
-  <h2 v-if="number < 0">The Number is Negative</h2>
-  <h2 v-else-if="number === 0">The Number is Zero</h2>
-  <h2 v-else-if="number > 0">The Number is Positive</h2>
-  <h2 v-else>Not a number</h2>
+  <!-- <h2 v-for="(name, index) in fullnames" :key="index" >{{name.first}} {{name.last}}</h2>  iterating in array of objects --> 
 
-  <template v-if="display">
-    <h2>Andro Eugenio VueJs</h2>
+  <!-- <div v-for="(actor, index) in actors" :key="index" >
+    <h2> {{index + 1}}. {{actor.name}} </h2>
+    <h2 v-for="(movie, index) in actor.movies" :key="index" > * {{movie}}</h2> iterating in array of objects with array props
+  </div> -->
+
+  <!-- <h2 v-for="(value, key, index) in myInfo" :key="index">{{key}} {{value}}</h2>  iterating with object with key and values -->
+
+  <template  v-for="(name, index) in names" :key="index"  >  
+    <h2 v-show="name === 'andro'">{{index}} {{name}}</h2>
   </template>
-
-  <h2 v-show="showElement">Using V-show</h2>
-  <!-- v-show only set display:none to css properties if the current condition is not truthy  -->
-
-  <h2 v-if="showElement">Using V-show</h2>
-  <!-- v-if  will completely inject to dom when condition is not truthy  -->
-
-  <!-- <h2> // long term
-    {{
-      number < 0 
-        ? `The number is negative`
-        : number === 0
-        ? `The number is zero`
-        : ` The number is positive` 
-    }}
-    {{ number }} 
-  </h2> -->
+  
 </template>
 
 <script>
@@ -33,11 +21,42 @@ export default {
   name: "App",
   data() {
     return {
-      number: 0,
-      display: true,
-      showElement: false,
+     names: ['andro', 'jean', 'john' ],
+     fullnames: [
+      {
+        first: 'andro', last: 'eugenio'
+      },
+      {
+        first: 'jean', last: 'correa'
+      },
+      {
+        first: 'john', last: 'doe'
+      },
+     ],
+     actors: [
+      {
+        name: 'andro eugenio',
+        movies: ['ang provinsyano', 'john wick']
+      },
+      {
+        name: 'jean correa',
+        movies: ['once upon a time', 'horror']
+      },
+      {
+        name: 'john doe',
+        movies: ['hotdog', '007']
+      },
+     ],
+     myInfo: {
+      name: "andro",
+      channel: 'androPyro',
+      course: 'vue 3'
+     }
     };
   },
+  methods: {
+    
+  }
 };
 </script>
 
