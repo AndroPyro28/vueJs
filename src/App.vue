2a81b20d0cc2e8706e4706be45864a108cc08649
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-13 05:05:13 -0400

    Component and Props

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,27 @@
 <template>
-  <h2>methods {{ fullname }}</h2>
-  <h2>computed {{ firstname }} {{ lastname }}</h2>
-  <button @click="changeName('jean correa')">change name</button>
-  <button @click="someValue += 1">{{ someValue }}</button>
-
-  <h2>Volume {{ volume }}</h2>
-  <button @click="volume += 1">Increase volume</button>
-  <button @click="volume -= 1">Decrease volume</button>
-  <button @click="someArray.push('...')" >Add ....</button>
+<!-- <Greet name="Andro" title="Programmer" />
+<Greet name="Jean" title="Police" />
+<Greet name="John" title="Analyst" />
+<Greet :name="name" :title="channel" /> -->
+<Article :title="'Article Title'" :likes="50" :isPublished="false" />
 </template>
 
 <script>
+// import Greet from './components/Greet.vue'
+import Article from "./components/Article.vue"
 export default {
-  name: "App",
+  name: 'App',
+  components: {
+    // Greet,
+    Article
+  },
   data() {
     return {
-      someValue: 0,
-      firstname: "andro",
-      lastname: "eugenio",
-      volume: 0,
-      someArray: []
-    };
-  },
-
-  methods: {
-    changeName(value) {
-      this.fullname = value;
-    },
-  },
-
-  computed: {
-    // computed will only rerender when the data value changed if its not change it will only use its cached value
-    fullname: {
-      get() {
-        return `${this.firstname} ${this.lastname}`;
-      },
-      set(value) {
-        const names = value.split(" ");
-        this.firstname = names[0];
-        this.lastname = names[1];
-      },
-    },
-  },
-
-  watch: {
-    volume: {
-      handler(newValue, oldValue) {
-        if( oldValue < newValue && newValue === 16) {
-          alert('volume may damage your hearing')
-        }
-      },
-      immediate: true,
-    },
-    someArray: {
-      handler(newValue, oldValue) {
-      console.log('someArray', {newValue, oldValue});
-      },
-      immediate: true,
-      deep: true
+      name: 'vishwas',
+      channel: 'Codevolution'
     }
-  },
-};
+  }
+}
 </script>
 
 <style>
@@ -68,7 +29,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Greet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>hello {{name}} a.k.a {{title}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GreetVue',
+        props: ["name", "title"]
+    }
+</script>
+
+<style scoped>
+
+</style>
