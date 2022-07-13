993bf1eb83aeb86902a6a093bfe530d10c75ac3f
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-13 04:04:04 -0400

    computed and watchers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <h2 v-once>{{name}}</h2>
-  <button @click="name = 'batman'" >change name</button>
-  <h2 v-pre> {{ name }}</h2>
+  <h2>methods {{ fullname }}</h2>
+  <h2>computed {{ firstname }} {{ lastname }}</h2>
+  <button @click="changeName('jean correa')">change name</button>
+  <button @click="someValue += 1">{{ someValue }}</button>
+
+  <h2>Volume {{ volume }}</h2>
+  <button @click="volume += 1">Increase volume</button>
+  <button @click="volume -= 1">Decrease volume</button>
+  <button @click="someArray.push('...')" >Add ....</button>
 </template>
 
 <script>
@@ -9,20 +15,51 @@ export default {
   name: "App",
   data() {
     return {
-        name: 'vishwas'
-      }
-    },
+      someValue: 0,
+      firstname: "andro",
+      lastname: "eugenio",
+      volume: 0,
+      someArray: []
+    };
+  },
+
   methods: {
-    displayName () {
-      alert(this.name)
+    changeName(value) {
+      this.fullname = value;
     },
-    submitForm () {
-      console.log('form values', this.formValues)
+  },
+
+  computed: {
+    // computed will only rerender when the data value changed if its not change it will only use its cached value
+    fullname: {
+      get() {
+        return `${this.firstname} ${this.lastname}`;
+      },
+      set(value) {
+        const names = value.split(" ");
+        this.firstname = names[0];
+        this.lastname = names[1];
+      },
+    },
+  },
+
+  watch: {
+    volume: {
+      handler(newValue, oldValue) {
+        if( oldValue < newValue && newValue === 16) {
+          alert('volume may damage your hearing')
+        }
+      },
+      immediate: true,
+    },
+    someArray: {
+      handler(newValue, oldValue) {
+      console.log('someArray', {newValue, oldValue});
+      },
+      immediate: true,
+      deep: true
     }
   },
-  watch: {
-    
-  }
 };
 </script>
 
@@ -35,6 +72,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
-
 </style>
